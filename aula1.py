25723a01ece189e272fd6b91433767c0f454815a
Alexsandro Macedo <alexsandromacedo872@gmail.com>
2025-12-10 12:42:59 -0300

    Primeiro commit

---
+++ aula1.py
@@ -0,0 +1,17 @@
+"""
+Docstring for aula1
+
+"""
+
+'''
+Usar para escrever minhas notas
+
+'''
+
+
+# Permite escrever um comentário
+print(123) # Na frente
+# Abaixo 
+print(456)
+# Acima
+
---
+++ aula3.py
@@ -0,0 +1,24 @@
+"""
+Docstring for aula3
+
+Python = Linguagem de programação 
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+
+print(1234)
+
+# Aspas simples
+print('Eu mesmo')
+print(1,'Eu "mesmo"')
+
+# Aspas duplas
+print("Eu mesmo")
+print(2,"Eu 'mesmo'")
+
+# Escape
+print("Eu \"mesmo\"")
+
+# r
+print(r"Eu \"mesmo\"")
